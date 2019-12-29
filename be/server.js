a342ac51e069c1cbcd77f4af6f8b7ed7bc7b1f24
4wangyu <4wangyu@gmail.com>
2019-12-29 17:30:08 +0800

    update express config

--- be/server.js
+++ be/server.js
@@ -4,8 +4,23 @@ const cors = require("cors");
 const { pool } = require("./config");
 const path = require("path");
 
+// Setup express
 const app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,HEAD,DELETE,OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization ,Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, "../fe/build")));
