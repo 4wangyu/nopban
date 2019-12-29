dd6ee9ebe6dc879bc69aba0ce0f3b152370166d4
4wangyu <4wangyu@gmail.com>
2019-12-29 17:11:11 +0800

    add production config for knex

--- knexfile.js
+++ knexfile.js
@@ -10,5 +10,15 @@ module.exports = {
     seeds: {
       directory: __dirname + "/db/seeds"
     }
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + "/db/migrations"
+    },
+    seeds: {
+      directory: __dirname + "/db/seeds"
+    }
   }
 };
