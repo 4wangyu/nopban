adb03533aecd95809eab389dd85c81170939f156
4wangyu <4wangyu@gmail.com>
2020-05-03 15:58:59 +0800

    keep user logged in

--- fe/src/App.js
+++ fe/src/App.js
@@ -56,6 +56,21 @@ const App = () => {
   // };
 
   const [context, dispatch] = React.useReducer(reducer, initialState);
+  React.useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user") || null);
+    const token = JSON.parse(localStorage.getItem("token") || null);
+
+    if (user && token) {
+      dispatch({
+        type: "LOGIN",
+        payload: {
+          user,
+          token,
+        },
+      });
+    }
+  }, []);
+
   return (
     <AppContext.Provider
       value={{
