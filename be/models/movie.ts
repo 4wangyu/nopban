4f5a2c41e50002ed71d68af8e870ab8438c55108
4wangyu <4wangyu@gmail.com>
2020-05-30 23:01:16 +0800

    make puppet work

--- be/models/movie.ts
+++ be/models/movie.ts
@@ -1,18 +1,28 @@
-import axios, { AxiosError } from 'axios';
 import { Request, Response } from 'express';
+import fs from 'fs';
+import puppet from '../puppet';
 
-const searchMovie = (request: Request, response: Response) => {
+const url = 'https://search.douban.com/movie/subject_search?search_text=';
+
+const searchMovie = async (request: Request, response: Response) => {
   const searchKey = encodeURI(request.query.searchkey as string);
-  axios
-    .get('https://movie.douban.com/j/subject_suggest?q=' + searchKey)
-    .then((res) => {
-      response.status(200).json(res.data);
-    })
-    .catch((e: AxiosError) => {
-      response.status(400).json({
-        error: e.message,
-      });
-    });
+
+  const page = await puppet();
+
+  try {
+    await page.goto(url + searchKey);
+    await page.waitForSelector('#root');
+    const bodyHTML = await page.evaluate(
+      () => document.getElementById('root').innerHTML
+    );
+    fs.writeFileSync('search.html', bodyHTML);
+
+    await page.close();
+    response.status(200).json({ html: 'success' });
+  } catch (e) {
+    console.warn(e);
+    response.status(500).json({ error: 'Error in fetching search results' });
+  }
 };
 
 export default searchMovie;
