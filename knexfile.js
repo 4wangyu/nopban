56c366d8a47a037ae1a6c10ced1d4f2a976bef98
4wangyu <4wangyu@gmail.com>
2020-05-31 15:11:11 +0800

    reformat

--- knexfile.js
+++ knexfile.js
@@ -1,31 +1,31 @@
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
   development: {
-    client: "pg",
+    client: 'pg',
     connection: {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      charset: "utf8",
+      charset: 'utf8',
     },
     migrations: {
-      directory: __dirname + "/db/migrations",
+      directory: __dirname + '/db/migrations',
     },
     seeds: {
-      directory: __dirname + "/db/seeds",
+      directory: __dirname + '/db/seeds',
     },
   },
   production: {
-    client: "pg",
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: __dirname + "/db/migrations",
+      directory: __dirname + '/db/migrations',
     },
     seeds: {
-      directory: __dirname + "/db/seeds",
+      directory: __dirname + '/db/seeds',
     },
   },
 };
