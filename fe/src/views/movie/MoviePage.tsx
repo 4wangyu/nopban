a8a377b694e147e6787cdf69e863218c47819942
4wangyu <4wangyu@gmail.com>
2020-06-09 00:30:37 +0800

    clean up imports

--- fe/src/views/movie/MoviePage.tsx
+++ fe/src/views/movie/MoviePage.tsx
@@ -2,10 +2,10 @@ import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { AppContext } from '../../App';
-import { arrToStr, getImdbIdFromUrl, getName } from '../../lib/util';
+import { MOVIE_URL } from '../../lib/constant';
+import { arrToStr, getImdbIdFromUrl } from '../../lib/util';
 import { Movie } from '../../models/movie.model';
 import './movie-page.scss';
-import { MOVIE_URL } from '../../lib/constant';
 
 function MoviePage() {
   const { movieId } = useParams();
