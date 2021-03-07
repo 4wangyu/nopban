67eae604a115cf1597574d22ad3a404c3ac751b3
4wangyu <4wangyu@gmail.com>
2021-03-08 00:08:36 +0800

    fix deploy to heroku

--- knexfile.js
+++ knexfile.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: { connectionString: process.env.DATABASE_URL, ssl: true },
     migrations: {
       directory: __dirname + '/db/migrations',
     },
