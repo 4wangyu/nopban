31ab252c90040f856aa28c9cd7be16f8b037a532
44sisi <luo.siwei@u.nus.edu>
2021-04-06 20:11:07 +0800

    update validateCategory

--- be/middleware/validate.ts
+++ be/middleware/validate.ts
@@ -1,10 +1,9 @@
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import { CATEGORIES } from '../lib/constant';
 
 const validateCategory = (req: Request, res: Response, next: NextFunction) => {
-  const CATEGORY_LIST = ['book', 'movie', 'music'];
-
-  let category = req.query.category as string;
-  if (CATEGORY_LIST.includes(category)) {
+  const category = req.query.category as string;
+  if (CATEGORIES.includes(category)) {
     next();
   } else {
     return res.status(404).json({
