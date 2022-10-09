16078bca69c3f2da69b4c20d5757cdb2cbc97324
4wangyu <4wangyu@gmail.com>
2022-10-09 13:20:16 +0800

    get dynamic cookie from gh

--- be/axios.ts
+++ be/axios.ts
@@ -1,12 +1,25 @@
 import axios from 'axios';
-
-const cookie =
-  'push_noty_num=0; push_doumail_num=0; douban-fav-remind=1; douban-profile-remind=1; viewed="27080432_30424101_35942057_35724868_24157160_35710226_26309661_35374360_35710878_35710421"; bid=XgAQ4SaJbkM; ll="108231"; ct=y; dbcl2="146985475:OXJbW7D5BEo"; ck=Cnbx';
+import https from 'https';
 
 axios.interceptors.request.use(
-  (config) => {
-    config.headers.cookie = cookie;
-    return config;
+  async (config) => {
+    return new Promise((resolve, reject) => {
+      https
+        .get(process.env.COOKIE, function (res) {
+          const bodyChunks: Uint8Array[] = [];
+          res
+            .on('data', function (chunk) {
+              bodyChunks.push(chunk);
+            })
+            .on('end', function () {
+              config.headers.cookie = Buffer.concat(bodyChunks);
+              resolve(config);
+            });
+        })
+        .on('error', function (e) {
+          reject(e);
+        });
+    });
   },
   (error) => {
     return Promise.reject(error);
